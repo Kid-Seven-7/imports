762b61ea6960eee7ab063173287f2494775c6307
Joseph L. Ngoma <testing.jo.wtc@gmail.com>
2018-10-14 18:28:40 +0200

    init

---
+++ sounds/statsScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class statsScript : MonoBehaviour {
+
+	public GameObject canvas;
+
+	public Button button;
+
+	public Slider slider;
+
+	float time = 0;
+
+	bool visible = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		// Debug.Log(slider.value);
+
+		if (Input.GetKey(KeyCode.C)){
+			Debug.Log("c clicked");
+			canvas.SetActive(false);
+		}
+	}
+}
